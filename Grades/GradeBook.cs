d85ec00eb1d81a41e7e0ac650ba6f781b01da637
Nenyi Arkaah <nenyi.arkaah@gmail.com>
2017-05-13 15:11:36 +0100

    Classes and Objects

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games
+{
+    public class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        private List<float> grades;
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+    }
+}
---
+++ Grades/GradeStatistics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games
+{
+    public class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            AverageGrade = 0;
+            LowestGrade = float.MaxValue;
+            HighestGrade = float.MinValue;
+        }
+
+
+        public float AverageGrade;
+        public float LowestGrade;
+        public float HighestGrade;
+    }
+}
